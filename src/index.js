921e0e6296f2bc3a5626c559d5414c24f7e59c99
Bill Wallace <wayfarer3130@gmail.com>
2022-02-16 14:50:25 -0500

    fix(config-point):Added getConfig to exports

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import { ConfigPoint } from './ConfigPoint';
 import { ConfigPointOperation, SortOp, ReferenceOp, ReplaceOp, DeleteOp, InsertOp, safeFunction } from './ConfigPointOperation';
 export default ConfigPoint;
 const register = ConfigPoint.register.bind(ConfigPoint);
+const getConfig = ConfigPoint.getConfig.bind(ConfigPoint);
 
 export {
   ConfigPoint, ConfigPointOperation, ReplaceOp, SortOp, ReferenceOp, DeleteOp, InsertOp, safeFunction,
